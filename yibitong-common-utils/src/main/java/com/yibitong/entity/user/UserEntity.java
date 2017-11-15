59c8e9e5c5d32281010b7ab6efb587e84abce4b0
yy <530803606@qq.com>
2017-11-15 15:22:43 +0800

    yibitong 项目框架

--- yibitong-common-utils/src/main/java/com/yibitong/entity/user/UserEntity.java
+++ yibitong-common-utils/src/main/java/com/yibitong/entity/user/UserEntity.java
@@ -10,53 +10,17 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@TableName("yzj_t_user")
+@TableName("customer_user")
 public class UserEntity extends Model<UserEntity>{
 
     @TableId
     private long user_id;
 
-    @TableField("user_phone")
-    private String user_phone;
+    @TableField("username")
+    private String username;
 
     @TableField("password")
     private String password;
-
-    @TableField("user_type")
-    private int user_type;
-
-    @TableField("user_bespeak")
-    private int user_bespeak;
-
-    @TableField("user_consult")
-    private int user_consult;
-
-    @TableField("user_head")
-    private String user_head;
-
-    @TableField("user_email")
-    private String user_email;
-
-    @TableField("user_sex")
-    private int user_sex;
-
-    @TableField("user_age")
-    private int user_age;
-
-    @TableField("jpushtag")
-    private String jpushtag;
-
-    @TableField("createtime")
-    private long createtime;
-
-    @TableField("reg_ip")
-    private String reg_ip;
-
-    @TableField("forbidden")
-    private int forbidden;
-
-    @TableField("recommend")
-    private long recommend;
 
     @Override
     protected Serializable pkVal() {
--- yibitong-user-server/src/main/java/yibitong/user/controller/TestController.java
+++ yibitong-user-server/src/main/java/yibitong/user/controller/TestController.java
@@ -23,21 +23,8 @@ public class TestController {
     @RequestMapping("/save")
     public String save(){
         UserEntity userEntity = new UserEntity();
-        userEntity.setUser_phone("15923512161");
+        userEntity.setUsername("测试项目");
         userEntity.setPassword("123455");
-        userEntity.setJpushtag("1111");
-        userEntity.setCreatetime(new Date().getTime());
-        userEntity.setForbidden(1);
-        userEntity.setRecommend(1);
-        userEntity.setReg_ip("127.0.0.1");
-        userEntity.setUser_age(10);
-        userEntity.setUser_consult(100);
-        userEntity.setUser_type(1);
-        userEntity.setForbidden(0);
-        userEntity.setRecommend(11);
-        userEntity.setUser_email("55555@qq.com");
-        userEntity.setUser_sex(1);
-        userEntity.setUser_bespeak(100);
         userService.insert(userEntity);
         return "success";
     }
